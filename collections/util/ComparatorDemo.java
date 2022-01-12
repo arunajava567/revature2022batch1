333e6b1ea977a1b622a713a7e6d4597a173d4f28
arunajava567 <30722352+arunajava567@users.noreply.github.com>
2022-01-12 17:48:42 +0530

    Add files via upload

--- collections/util/ComparatorDemo.java
+++ collections/util/ComparatorDemo.java
@@ -1,40 +1,85 @@
-import java.util.*;
+package com.revature.collections;
 
-public class ComparatorDemo
-{
-	public static void main(String args[])
-	{
-		TreeSet t = new TreeSet(new MyComparator());
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class Employee {
+	Integer id;
+	String name;
+	Double sal;
 	
-		t.add(new Integer(10));
-		t.add(new Integer(15));
-		t.add(new Integer(5));
-		t.add(new Integer(0));
-		t.add(new Integer(2));
+	public Employee(Integer id, String name, Double sal) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sal = sal;
+	}
 
-		System.out.println(t);
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", sal=" + sal + "]";
 	}
 }
 
-class MyComparator implements Comparator
-{
-	public int compare(Object obj1,Object obj2)
-	{
-		Integer I1 = (Integer)obj1;
-		Integer I2 = (Integer)obj2;
+class IdComparator implements Comparator{
+	
+	public int compare(Object o1,Object o2) {
+		Employee e1=(Employee)o1;
+		Employee e2=(Employee)o2;
+		 if (e1.id>e2.id)
+			 return +1; 
+		 else if(e1.id<e2.id) 
+			 return -1; 
+		 else 
+			 return  0;
 		
-		int i1 = I1.intValue();
-		int i2 = I2.intValue();
-
-		if(i1<i2)
-		{
-			return -1;
-		}
-		else if(i1>i2)
-		{
-			return +1;
-		}
-		else
-			return 0;
 	}
+}
+
+
+class NameComparator implements Comparator{
+	public int compare(Object o1,Object o2) {
+		Employee e1=(Employee)o1;
+		Employee e2=(Employee)o2;
+		return e1.name.compareTo(e2.name);
+		
+	}
+	
+}
+class SalaryComparator implements Comparator{
+	public int compare(Object o1,Object o2) {
+		Employee e1=(Employee)o1;
+		Employee e2=(Employee)o2;
+		 if (e1.sal>e2.sal)
+			 return +1; 
+		 else if(e1.sal<e2.sal) 
+			 return -1; 
+		 else 
+			 return  0;
+		
+	}
+	
+}
+
+public class ComparatorDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Employee> elist=new ArrayList<Employee>();
+		
+		elist.add(new Employee(10,"bindu",89786.90));
+		elist.add(new Employee(9,"kiran",79786.90));
+		elist.add(new Employee(05,"manisha",99786.90));
+		elist.add(new Employee(20,"amar",88786.90));
+		
+		System.out.println(elist);
+		Collections.sort(elist,new NameComparator());
+		System.out.println(elist);
+		Collections.sort(elist,new IdComparator());
+		System.out.println(elist);
+		Collections.sort(elist,new SalaryComparator());
+		System.out.println(elist);
+
+	}
+
 }
